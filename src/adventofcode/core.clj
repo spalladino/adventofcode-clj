0a5a7fc77d7cbb16c325f7311afddb1015bee427
Santiago Palladino <spalladino@gmail.com>
2015-12-20 21:03:29 -0300

    Dynamically require code from all days

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,40 +1,17 @@
-(ns adventofcode.core
-  (:require [adventofcode.day01 :as day01])
-  (:require [adventofcode.day02 :as day02])
-  (:require [adventofcode.day03 :as day03])
-  (:require [adventofcode.day04 :as day04])
-  (:require [adventofcode.day05 :as day05])
-  (:require [adventofcode.day06 :as day06])
-  (:require [adventofcode.day07 :as day07])
-  (:require [adventofcode.day08 :as day08])
-  (:require [adventofcode.day09 :as day09])
-  (:require [adventofcode.day10 :as day10])
-  (:require [adventofcode.day11 :as day11])
-  (:require [adventofcode.day12 :as day12])
-  (:require [adventofcode.day13 :as day13])
-  (:require [adventofcode.day14 :as day14])
-  (:require [adventofcode.day15 :as day15])
-  (:require [adventofcode.day16 :as day16])
-  (:require [adventofcode.day17 :as day17]))
+(ns adventofcode.core)
+
+(def DAYS 17)
+
+(doall
+  (map (fn [day] (eval `(require '[~(symbol (format "adventofcode.day%02d" (inc day))) :as ~(symbol (format "day%02d" (inc day)))])))
+  (range DAYS)))
 
 (def days
-  {1  day01/main
-   2  day02/main
-   3  day03/main
-   4  day04/main
-   5  day05/main
-   6  day06/main
-   7  day07/main
-   8  day08/main
-   9  day09/main
-   10 day10/main
-   11 day11/main
-   12 day12/main
-   13 day13/main
-   14 day14/main
-   15 day15/main
-   16 day16/main
-   17 day17/main})
+  (apply hash-map
+    (flatten
+      (map
+        (fn [day] [(inc day) (eval (symbol (format "day%02d/main" (inc day))))])
+        (range DAYS)))))
 
 (defn -main [arg]
   (let [day (Integer/parseInt arg)]
