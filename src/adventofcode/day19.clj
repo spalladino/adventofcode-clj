13dc73eb2aa5346b8f6e185787a2f0d7aa6f02fc
Santiago Palladino <spalladino@gmail.com>
2015-12-25 12:51:19 -0300

    Day 19 p2

--- src/adventofcode/day19.clj
+++ src/adventofcode/day19.clj
@@ -11,10 +11,9 @@
     (group-by key-fn)
     (reduce-kv #(assoc %1 %2 (map val-fn %3)) {})))
 
-(defn parse-lines [[source & expansions]]
-  {:source source
+(defn parse-lines [[molecule & expansions]]
+  {:molecule molecule
    :expansions (map parse-line expansions)})
-  ;  :expansions (parse-expansions expansions first last)})
 
 (defn parse-data [data]
   (->> data
@@ -31,9 +30,61 @@
         (cons replaced
           (map (partial str head) (replacements [lft rgt] tail)))))))
 
-(defn calibration [{source :source expansions :expansions}]
-  (dec (count (distinct (mapcat #(replacements % source) expansions)))))
+(defn calibration [{molecule :molecule expansions :expansions}]
+  (->> expansions
+    (mapcat #(replacements % molecule))
+    (distinct)
+    (count)
+    (dec)))
+
+(def expand-molecule
+  (memoize (fn [expansions molecule]
+    (distinct (mapcat #(replacements % molecule) expansions)))))
+
+(defn generate-molecules-bfs [expansions molecules target]
+  (let [next-gen (->> molecules
+                   (mapcat (partial expand-molecule expansions))
+                   (distinct)
+                   (remove (set molecules)))]
+    (if (some #(= % target) next-gen) 1
+        (inc (generate-molecules-bfs expansions next-gen target)))))
+
+(defn deconstruct-molecule-heur [expansions molecule]
+  (if-let [[lft rgt] (first (filter #(.contains molecule (last %)) expansions))]
+    (let [match-count (count (re-seq (re-pattern rgt) molecule))
+          replaced (str/replace molecule rgt lft)
+          [goal iters] (deconstruct-molecule-heur expansions replaced)]
+      (list goal (+ match-count iters)))
+    (list molecule 0)))
+
+(defn deconstruct-molecule-dfs [expansions molecule]
+  (if (= molecule "e")
+    [0]
+    (->> expansions
+      (filter #(.contains molecule (last %)))
+      (map (fn [[lft rgt]]
+             (let [match-count (count (re-seq (re-pattern rgt) molecule))
+                  replaced (str/replace molecule rgt lft)
+                  deconstructions (deconstruct-molecule-dfs expansions replaced)]
+                (if-let [iters (first (remove nil? deconstructions))]
+                  (+ match-count iters)
+                  nil)))))))
+
+(defn generation-bfs [{molecule :molecule expansions :expansions}]
+  (generate-molecules-bfs expansions ["e"] molecule))
+
+(defn generation-heur [{molecule :molecule expansions :expansions}]
+  (deconstruct-molecule-heur
+    (sort-by (comp count last) > expansions)
+    molecule))
+
+(defn generation-dfs [{molecule :molecule expansions :expansions}]
+  (first
+    (deconstruct-molecule-dfs
+      (sort-by (comp count last) > expansions)
+      molecule)))
 
 (defn main []
   (let [data (parse-data (slurp "./inputs/day19.txt"))]
-    (println "Distinct molecules: " (calibration data))))
+    (println "Distinct molecules:   "    (calibration data))
+    (println "Minimum replacements to: " (generation-dfs data))))
--- test/adventofcode/day19_test.clj
+++ test/adventofcode/day19_test.clj
@@ -11,3 +11,75 @@ O => HH
 
 HOH"]
       (is (= 4 (day19/calibration (day19/parse-data data)))))))
+
+(deftest test-generation-bfs
+    (testing "generation bfs"
+      (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOH"]
+        (is (= 3 (day19/generation-bfs (day19/parse-data data)))))))
+
+(deftest test-generation-bfs-2
+      (testing "generation bfs favourite molecule"
+        (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOHOHO"]
+          (is (= 6 (day19/generation-bfs (day19/parse-data data)))))))
+
+(deftest test-generation-heur
+    (testing "generation heur"
+      (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOH"]
+        (is (= '("e" 3) (day19/generation-heur (day19/parse-data data)))))))
+
+(deftest test-generation-heur-2
+      (testing "generation heur favourite molecule"
+        (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOHOHO"]
+          (is (= '("e" 6) (day19/generation-heur (day19/parse-data data)))))))
+
+(deftest test-generation-dfs
+    (testing "generation dfs"
+      (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOH"]
+        (is (= 3 (day19/generation-dfs (day19/parse-data data)))))))
+
+(deftest test-generation-dfs-2
+      (testing "generation dfs favourite molecule"
+        (let [data "
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOHOHO"]
+          (is (= 6 (day19/generation-dfs (day19/parse-data data)))))))
