2043c807d76cf3e87ba504efa8654f242a616a23
Santiago Palladino <spalladino@gmail.com>
2015-12-29 16:10:47 -0300

    Day 24 p1

--- src/adventofcode/day24.clj
+++ src/adventofcode/day24.clj
@@ -1,8 +1,9 @@
 (ns adventofcode.day24
   (:require [clojure.string :as str])
+  (:require [clojure.set :as cset])
   (:require [clojure.math.combinatorics :as combo]))
 
-(defn p [x] (prn x) x)
+(def ^:dynamic *group-count* 3)
 
 (defn parse-data [data]
   (->> data
@@ -14,16 +15,32 @@
   (let [min-val (apply min (map fun values))]
     (filter #(= (fun %) min-val) values)))
 
+(defn can-split? [packages target]
+  (some
+    #(= target (apply + (first %)))
+    (combo/partitions packages :min (dec *group-count*) :max (dec *group-count*))))
+
+(defn first-groups-sized [packages size]
+  (let [target-weight (/ (apply + packages) *group-count*)]
+    (->> size
+      (combo/combinations packages)
+      (filter #(= target-weight (apply + %))))))
+
+(defn first-groups [packages]
+  (->> (range)
+    (map (partial first-groups-sized packages))
+    (filter not-empty)
+    (first)))
+
 (defn best-arrangement [packages]
-  (let [target-weight (/ (apply + packages) 3)
-        partitions (combo/partitions packages :min 3 :max 3)]
-    (->> partitions
-      (filter (partial every? #(= (apply + %) target-weight)))
-      (apply concat)
-      (apply min-keys count)
+  (let [target-weight (/ (apply + packages) *group-count*)]
+    (->> packages
+      (first-groups)
+      (filter #(can-split? (vec (cset/difference (set packages) (set %))) target-weight))
       (map (partial apply *))
       (apply min))))
 
 (defn main []
   (let [packages (parse-data (slurp "./inputs/day24.txt"))]
-    (println "Part 1: " (best-arrangement packages))))
+    (println "Groups of 3: " (best-arrangement packages))
+    (println "Groups of 4: " (binding [*group-count* 4] (best-arrangement packages)))))
